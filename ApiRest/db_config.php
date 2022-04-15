5d89b9fab697fd4a577b072b7b416b3e6274d831
Elizabeth Zapata <lenguaje.pecz@gmail.com>
2022-04-15 11:00:43 -0500

    Se configura la conexion con la base de datos

--- ApiRest/db_config.php
+++ ApiRest/db_config.php
@@ -1,3 +1,23 @@
 <?php
 
-echo "Se realiza prueba de ramas";
+class DBConfig{
+
+    private $user = "root";
+    private $password = "";
+    private $dbName = "simple_invoice";
+    private $host = "localhost";
+
+    public function connect(){
+        try{
+            $dsn = "mysql:host=$this->host;dbname=$this->dbName";
+            $connection = new PDO($dsn,$this->user, $this->password);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $connection;
+            echo "Conexión exitosa";
+        }catch(PDOException $exception){
+            echo "Error en la db". $exception->getMessage();
+        }
+    }
+}
+$dbConnection = new DBConfig();
+$dbConnection->connect(); 
