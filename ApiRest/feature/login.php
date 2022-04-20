28dfb6dbc1c3062b2c1f338b78ac605f76cca741
Elizabeth Zapata <lenguaje.pecz@gmail.com>
2022-04-19 22:50:20 -0500

    get request complete

--- ApiRest/feature/login.php
+++ ApiRest/feature/login.php
@@ -1,7 +1,12 @@
 <?php
-
-if( $_SERVER['REQUEST_METHOD'] == "POST"){
-    echo "todo bien";
+require_once(dirname(__DIR__)."../db/db_config.php");
+if( $_SERVER['REQUEST_METHOD'] == "GET"){
+    $user_id = $_GET['user_id'];
+    $db = new DBConfig();
+    $dbConnection = $db->connect();
+    $query = "SELECT * FROM users WHERE user_id=$user_id";
+    $users = $dbConnection->query($query)->fetchAll(PDO::FETCH_ASSOC);
+    echo(json_encode($users[0]));
 }
 else{
     echo "parce no se puede";
